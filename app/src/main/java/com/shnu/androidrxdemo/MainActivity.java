2c1c5d5744b136cf3b00a1b49cf8c44e4d52b87a
ShenDanLai <17721129316@163.com>
2018-11-20 07:33:55 +0800

    ***上游事件创建***

--- app/src/main/java/com/shnu/androidrxdemo/MainActivity.java
+++ app/src/main/java/com/shnu/androidrxdemo/MainActivity.java
@@ -3,15 +3,21 @@ package com.shnu.androidrxdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.shnu.animation.androidrxdemo.R;
 
+import org.reactivestreams.Subscription;
+
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableEmitter;
 import io.reactivex.FlowableOnSubscribe;
+import io.reactivex.FlowableSubscriber;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
@@ -32,23 +38,138 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText etCenter;
 
+    private TextView tvRequest;
+
+    public Subscription subscription;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etCenter = (EditText) findViewById(R.id.tv);
+//        etCenter = (EditText) findViewById(R.id.tv);
+
+//        rxFlowable01();
+    }
+
+
+    /**
+     * 使用timer创建上游事件
+     */
+
+    public void rxTimer(){
+        Observable.timer(2000,TimeUnit.MILLISECONDS).subscribe(new Consumer<Long>() {
+            @Override
+            public void accept(Long aLong) throws Exception {
+
+            }
+        });
+
+
 
     }
 
-    public void rxFlowable(){
+    /**
+     * 使用Interval 创建上游事件
+     */
+    public void rxInterval() {
+
+        Disposable disposable = Observable.interval(2, 3, TimeUnit.SECONDS).subscribe(new Consumer<Long>() {
+            @Override
+            public void accept(Long aLong) throws Exception {
+                Log.e(TAG, "EMITTER : " + aLong + " in thread------" + Thread.currentThread().getName());
+            }
+        });
+
+        //结束轮询
+        //disposable.dispose();
+    }
+
+
+    /**
+     * Flowable
+     */
+
+
+    public void rxFlowable01() {
+        tvRequest = (TextView) findViewById(R.id.tv_request);
+        tvRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                subscription.request(5);
+            }
+        });
+        Flowable<Integer> flowable = Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+                for (int i = 0; i < 100; i++) {
+                    emitter.onNext(i);
+                    Log.e(TAG, "EMITTER : " + i + " in thread------" + Thread.currentThread().getName());
+                }
+            }
+        }, BackpressureStrategy.MISSING);
+        FlowableSubscriber<Integer> subscriber = new FlowableSubscriber<Integer>() {
+
+            @Override
+            public void onSubscribe(final Subscription s) {
+                subscription = s;
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.e(TAG, "RECEIVE :" + integer + " in thread-----" + Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Log.e(TAG, "ERROR :" + t.toString());
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+        flowable.subscribe(subscriber);
+    }
+
+    /**
+     * 介绍RxJava 中最基本的线程使用方式；
+     */
+    public void rxJavaThread01() {
+
+        /**
+         * 上游事件，通过Schedule 控制线程
+         */
+        Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                emitter.onNext("Welcome to RxJava!");
+                Log.e(TAG, "EMITTER : in thread------" + Thread.currentThread().getName());
+            }
+        }).subscribeOn(AndroidSchedulers.mainThread())  //上游事件在主线程中
+                .observeOn(Schedulers.computation());   //下游事件在计算密集型的线程中
+
+        Consumer<String> consumer = new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.e(TAG, "RECEIVE :" + s + " in thread-----" + Thread.currentThread().getName());
+            }
+        };
+        observable.subscribe(consumer);
+    }
+
+
+    public void rxFlowable() {
         Flowable<String> flowable = Flowable.create(new FlowableOnSubscribe<String>() {
             @Override
             public void subscribe(FlowableEmitter<String> emitter) throws Exception {
 
             }
-        }, BackpressureStrategy.BUFFER).flatMap();
+        }, BackpressureStrategy.BUFFER);
 
-        Consumer consumer  = new Consumer<String>() {
+        Consumer consumer = new Consumer<String>() {
             @Override
             public void accept(String string) throws Exception {
 
@@ -56,14 +177,7 @@ public class MainActivity extends AppCompatActivity {
         };
 
 
-
     }
-
-
-
-
-
-
 
 
     /**
